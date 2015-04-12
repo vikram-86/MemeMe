35717f16d6a5914150d19102d8630fdf83e2c183
vikram-86 <suthananth.arulanantham@gmail.com>
2015-04-12 20:18:19 +0200

    Committed by Udacity

---
+++ MemeMe/MemeMe/MemeCollectionCell.swift
@@ -0,0 +1,16 @@
+//
+//  MemeCollectionCell.swift
+//  MemeMe
+//
+//  Created by Suthananth Arulanantham on 08.04.15.
+//  Copyright (c) 2015 Suthananth Arulanantham. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionCell: UICollectionViewCell {
+    
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var sentDate: UILabel!
+    
+}
